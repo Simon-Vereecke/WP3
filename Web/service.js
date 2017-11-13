f9a624ba1f3bc96ac3b8a5d0fa7ee3b005ad2166
Simon <simon.vereecke@student.pxl.be>
2017-11-13 21:07:28 +0100

    DELETE & POST toegevoegd

--- Web/service.js
+++ Web/service.js
@@ -1,28 +1,92 @@
+const BaseURL = 'http://192.168.33.22/REST/';
+
 window.addEventListener("load", handleWindowLoad);
 
 
 function handleWindowLoad() {
     let btnJSON = document.getElementById("btnjson");
+    let deleteBtn = document.getElementById("btnDeleteContact");
+    let btnAddContact = document.getElementById("btnAddContact");
     btnJSON.addEventListener("click", handleClickJson);
+    deleteBtn.addEventListener("click", handleClickDeleteContact);
+    btnAddContact.addEventListener("click", handleClickPostContact);
 }
 
 function handleClickJson() {
-    const URL = 'http://192.168.33.22/REST/contacts/';
+    const URL = BaseURL + 'contacts/';//'http://192.168.33.22/REST/contacts/';
     fetch(URL)
         .then( (response) => {return response.json();})   // json() of toJSON() gebruiken ???
         .then(function (data) {
             data.forEach(myFunction);
             // writeOutput(data.id + " " + data.name);// + " " + data.email);
         })
-        .catch( (exception) => {writeOutput(exception);});
+        .catch( (exception) => {writeAllContactsOutput(exception);});
 }
 
 function myFunction(contact){
-    writeOutput(contact.id + " " + contact.name + " " + contact.email);
+    writeAllContactsOutput(contact.id + " " + contact.name + " " + contact.email);
 }
 
-function writeOutput(text) {
+function handleClickDeleteContact() {
+    let id = document.getElementById("contactId").value;
+    const URL = BaseURL + 'contacts/';
+    fetch(URL+id, {
+        method: 'DELETE'
+        // body: id
+    })
+        .then( (response) => {return response.json();})   // toJSON() gebruiken ???
+        // .then(function (data) {
+        //     writeDeleteOutput(data.id + " " + data.name);
+        // })
+        //of
+        // .then( (response) => {
+        //     if(response.status == 201) {
+        //         return response.json();
+        //     } else {
+        //         throw response.status;
+        //     }
+        // })
+        // .then(function (data) {
+        //     writeOutput(data.id + " " + data.name);
+        // })
+        .catch( (exception) => {writeDeleteOutput(exception);});
+}
+
+function handleClickPostContact() {
+    const URL = BaseURL + 'contacts/';
+    let contactName = document.getElementById("contactName").value;
+    let contact = {name: contactName};
+    fetch(URL, {
+            method: "POST",
+            body: JSON.stringify(contact)
+        }
+    ).then( (response) => {
+        if(response.status == 200) { // stond eerst op 201
+            return response.json();
+        } else {
+            throw response.status;
+        }
+    })
+        .then(function (data) {
+            // writeAddContactOutput(data.id + " " + data.name);
+        })
+        .catch( (exception) => {writeAddContactOutput(exception.message);});
+}
+
+function writeAddContactOutput(text) {
     let textNode = document.createTextNode(text + "\n");
     let output = document.getElementById("output");
+    output.appendChild(textNode);
+}
+
+function writeDeleteOutput(text) {
+    let textNode = document.createTextNode(text + "\n");
+    let output = document.getElementById("output");
+    output.appendChild(textNode);
+}
+
+function writeAllContactsOutput(text) {
+    let textNode = document.createTextNode(text + "\n");
+    let output = document.getElementById("AllContactsOutput");
     output.appendChild(textNode);
 }
